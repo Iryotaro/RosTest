58c3dc96ed7b026230fb63927299f7374a08c5e7
Iryotaro <ryotarosyuzi0712@gmail.com>
2020-09-09 09:37:45 +0000

    Travis Test

--- travis_ros_install.bash
+++ travis_ros_install.bash
@@ -1,16 +1,15 @@
 #!/bin/bash -xve
 
-#required packages 
-sudo pip install catkin_pkg
-sudo pip install empy
-sudo pip install pyyaml
-sudo pip install rospkg
+#required packages
+pip install catkin_pkg
+pip install empy
+pip install pyyaml
+pip install rospkg
 
 #ros install
 cd ..
-git clone https://github.com/Iryotaro/ros_setup_scripts_Ubuntu18.04_desktop
-r.git
-cd ./ros_setup_scripts_Ubuntu18.04_desktop
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
 
@@ -21,3 +20,7 @@ source /opt/ros/indigo/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
+
+# Copyright 2016 Ryuichi Ueda
+# Released under the BSD License.
+# To make line numbers be identical with the book, this statement is written here. Don't move it to the header.
