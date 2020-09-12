4124b9ecd45a7a49d0067294cfd6ed8caaf22e60
Iryotaro <ryotarosyuzi0712@gmail.com>
2020-09-12 05:21:37 +0000

    Travis Test

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -1,13 +1,13 @@
 #!/bin/bash -xve
 
-#required packages
+#required packages 
 sudo pip install catkin_pkg
 sudo pip install empy
-sudo pip install pyyaml
+sudo pip install pyymal
 sudo pip install rospkg
 
 #ros install
-cd ../
+cd ..
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu18.04_desktop.git
 cd ./ros_setup_scripts_Ubuntu18.04_desktop
 bash ./step0.bash
@@ -15,7 +15,7 @@ bash ./step1.bash
 
 #catkin setup
 mkdir -p ~/RosTest/src
-cd ~/RosTest/src
+cd ~/catkin_ws/src
 source /opt/ros/indigo/setup.bash
 catkin_init_workspace
 cd ~/RosTest
